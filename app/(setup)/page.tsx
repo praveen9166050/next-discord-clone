b55788df08f9232533a7b5f8049c2723e4205639
Praveen Kumar <praveen9166050@gmail.com>
2024-08-31 23:29:32 +0530

    Initial Modal UI

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,5 +1,6 @@
+import InitialModal from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
-import { initialProfile } from "@/lib/initial-profile"
+import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
 
 export default async function SetupPage() {
@@ -20,6 +21,6 @@ export default async function SetupPage() {
     return redirect(`/servers/${server.id}`);
   }
   return (
-    <div>Create a server</div>
+    <InitialModal />
   )
 }
