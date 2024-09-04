34de2d09c1de30d629759f8b59de382f06e1e49e
Praveen Kumar <praveen9166050@gmail.com>
2024-09-04 22:16:55 +0530

    Edit Server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 import CreateServerModal from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
 import InviteModal from "@/components/modals/invite-modal";
+import EditServerModal from "@/components/modals/edit-server-modal";
 
 export default function ModalProvider() {
   const [isMounted, setIsMounted] = useState(false);
@@ -18,6 +19,7 @@ export default function ModalProvider() {
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
     </>
   )
 }
